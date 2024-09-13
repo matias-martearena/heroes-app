e310b2bfb3b901d860e374e98c79efd443e12818
Matias Martearena <mmartearena@asap-consulting.net>
2024-09-13 11:31:02 -0300

    add: styles for navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatToolbarModule],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  sidebarItems = [
+    { label: 'Home', icon: 'home', url: '/home' },
+    { label: 'Listado', icon: 'label', url: '/home/list-heroes' },
+    { label: 'Añadir', icon: 'add', url: '/home/heroes-form' },
+    { label: 'Buscar', icon: 'search', url: '/home/search-heroes' },
+  ]
+}
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core'
-import { NavbarComponent } from '../navbar/navbar.component'
-import { MatSidenavModule } from '@angular/material/sidenav'
 import { RouterOutlet } from '@angular/router'
-import { SidebarComponent } from '../sidebar/sidebar.component'
+
+import { MatSidenavModule } from '@angular/material/sidenav'
+
+import { NavbarComponent } from '../navbar/navbar.component'
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [NavbarComponent, MatSidenavModule, RouterOutlet, SidebarComponent],
+  imports: [NavbarComponent, MatSidenavModule, RouterOutlet],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
 })
