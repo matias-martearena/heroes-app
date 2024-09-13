e9f3be9d673b742c08ddc992166a1a56db2919c6
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-09-13 10:58:23 -0300

    Search bar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { SearchBarComponent } from '../search-bar/search-bar.component'
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatToolbarModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    SearchBarComponent,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
